81251c5b28835a29670639f081f835ce8e9bc6d1
Lyubomyr <bobboby403@gmail.com>
2023-02-26 21:00:19 +0200

    edited Spring Boot Security  using SecurityFilterChain

--- src/main/java/SpringBootUseThymeleaf/configuration/DemoDataSourceConfig.java
+++ src/main/java/SpringBootUseThymeleaf/configuration/DemoDataSourceConfig.java
@@ -1,11 +1,16 @@
 package SpringBootUseThymeleaf.configuration;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.*;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
@@ -34,6 +39,35 @@ public class DemoDataSourceConfig {
     @ConfigurationProperties(prefix="security.datasource")
     public DataSource securityDataSource() {
         return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
+
+        http.authorizeRequests()
+                .antMatchers("/employees/showAddPage*").hasAnyRole("MANAGER", "ADMIN")
+                .antMatchers("/employees/save*").hasAnyRole("MANAGER", "ADMIN")
+                .antMatchers("/employees/delete").hasRole("ADMIN")
+                .antMatchers("/employees/**").hasRole("EMPLOYEE")
+                .antMatchers("/resources/**").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/showMyLoginPage")
+                .loginProcessingUrl("/authenticateTheUser")
+                .permitAll()
+                .and()
+                .logout().permitAll()
+                .and()
+                .exceptionHandling().accessDeniedPage("/access-denied");
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsManager users(@Qualifier("securityDataSource") DataSource dataSourcsecurityDataSourcee){
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSourcsecurityDataSourcee);
+        return   users;
+
     }
 
 }
